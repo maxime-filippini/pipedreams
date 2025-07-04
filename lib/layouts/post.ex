3b8cb72c7259e467c0568155badf052855631d4a
Maxime FIlippini <50547094+maxime-filippini@users.noreply.github.com>
2025-07-04 11:03:11 +0200

    Update post.ex

--- lib/layouts/post.ex
+++ lib/layouts/post.ex
@@ -7,16 +7,15 @@ defmodule Layouts.Post do
     ~H"""
     <Main.main>
       <div class="flex flex-col gap-4 pb-4 mb-4 border-b border-violet-200">
-      <h1 class="pb-6 text-3xl font-semibold border-b border-violet-200">{@post.title}</h1>
-
+        <h1 class="pb-6 text-3xl font-semibold border-b border-violet-200">{@post.title}</h1>
         <p class="font-semibold">Tags</p>
-    <ul class="flex flex-wrap gap-4 mb-4">
+        <ul class="flex flex-wrap gap-4 mb-4">
           <li :for={tag <- @post.tags}>
             <a class="px-4 py-2 duration-200 rounded-lg bg-violet-100 hover:violet-200" href={"/tags/#{tag}.html"}>{tag}</a>
           </li>
         </ul>
       </div>
-      <article class="prose prose-violet sm:prose-xl">
+      <article class="prose prose-violet sm:prose-lg">
         <%= raw @post.body %>
       </article>
     </Main.main>
